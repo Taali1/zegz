3f93c62b89ff8e2bd0922a58b6a5ac2948c4d349
Taali <matrebacz125@gmail.com>
2024-10-09 07:25:04 +0200

    up

--- scripts.js
+++ scripts.js
@@ -1,16 +1,41 @@
 console.log("start")
-  
-let lastScrollTop = 0;
-const navbar = document.getElementById("menu");
 
-window.addEventListener("scroll", function() {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    
-    if (scrollTop > lastScrollTop) {
-        navbar.style.transform = "translateY(-100%)";
-    } else {
-        navbar.style.transform = "translateY(0)";
-    }
-    
-    lastScrollTop = scrollTop;
-});
+// gallery
+if (document.getElementById('gallery-block')) {
+  console.log('gallery')
+  const galleryItems = document.querySelectorAll('.gallery-item');
+  console.log(galleryItems)
+
+  galleryItems.forEach(function(galleryItem) {
+      galleryItem.addEventListener('mouseleave', function() {
+          galleryItem.classList.remove('gallery-item');
+          galleryItem.classList.add('gallery-item-invert');
+      });
+
+      galleryItem.addEventListener('mouseenter', function() {
+          galleryItem.classList.remove('gallery-item-invert');
+          galleryItem.classList.add('gallery-item');
+      });
+  });
+}
+
+// index
+if (document.getElementById('main-scroll')) {
+  console.log("main")
+  const sections = document.querySelectorAll('.section');
+  console.log(sections)
+
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('in-view');
+          } else {
+              entry.target.classList.remove('in-view');
+          }
+      });
+  }, { threshold: 0.5 });
+
+  sections.forEach(slide => {
+      observer.observe(slide);
+  });
+}
